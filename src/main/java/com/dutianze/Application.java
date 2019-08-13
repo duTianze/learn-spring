608822f4d50eebb8e50bf670e426f68aba67756f
杜天泽 <dutianzer@gmail.com>
2019-08-13 17:00:19 +0800

    aop test

--- src/main/java/com/dutianze/Application.java
+++ src/main/java/com/dutianze/Application.java
@@ -1,4 +1,4 @@
-package com.dutianze.c1;
+package com.dutianze;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/dutianze/c1/KnightMain.java
+++ src/main/java/com/dutianze/c1/KnightMain.java
@@ -1,16 +1,14 @@
 package com.dutianze.c1;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class KnightMain {
 
-  public static void main(String[] args) throws Exception {
-    ClassPathXmlApplicationContext context =
-        new ClassPathXmlApplicationContext(
-                "com/c1/knight.xml");
-    Knight knight = context.getBean(Knight.class);
-    knight.embarkOnQuest();
-    context.close();
+  public static void main(String[] args)  {
+      ApplicationContext ctx = new ClassPathXmlApplicationContext("minstrel.xml", Knight.class);
+      Knight knight = ctx.getBean(Knight.class);
+      knight.embarkOnQuest();
   }
 
 }
