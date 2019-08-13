91fe90ae7063ad62b5c61286ff1e79fcef785e4c
杜天泽 <dutianzer@gmail.com>
2019-08-13 20:36:07 +0800

    c1 finish

--- src/main/java/com/dutianze/c1/KnightMain.java
+++ src/main/java/com/dutianze/c1/KnightMain.java
@@ -1,13 +1,13 @@
 package com.dutianze.c1;
 
-import com.dutianze.c1.config.KnightConfig;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class KnightMain {
 
   public static void main(String[] args)  {
-      //ApplicationContext ctx = new ClassPathXmlApplicationContext("minstrel.xml", Knight.class);
-      AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(KnightConfig.class);
+      ApplicationContext ctx = new ClassPathXmlApplicationContext("minstrel.xml", Knight.class);
+      //AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(KnightConfig.class);
       Knight knight = ctx.getBean(Knight.class);
       knight.embarkOnQuest();
   }
